2ba214c3eed076150f035d76bea5fcac98d5263b
Adam Barča <xbarca@fi.muni.cz>
2023-03-06 18:31:56 +0100

    INIT

---
+++ src/main/java/cz/muni/fi/pa165/icehockeymanager/IceHockeyManagerApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.icehockeymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceHockeyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IceHockeyManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/muni/fi/pa165/icehockeymanager/IceHockeyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.icehockeymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceHockeyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
