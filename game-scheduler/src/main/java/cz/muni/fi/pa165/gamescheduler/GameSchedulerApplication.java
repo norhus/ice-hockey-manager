4b43ef59b33fca0e07e0a0f302d265ff3a2ced0f
Norbert Husarčík <xhusarc@fi.muni.cz>
2023-03-22 12:01:33 +0100

    Created game-scheduler microservice

---
+++ game-scheduler/src/main/java/cz/muni/fi/pa165/gamescheduler/GameSchedulerApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.gamescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameSchedulerApplication.class, args);
+    }
+
+}
