bccebcdbbcde08e7851d7107618375ff35e92b6b
Ján Homola <xhomola@fi.muni.cz>
2023-03-22 18:41:57 +0100

    IMPL match

---
+++ model/src/main/java/cz/muni/fi/pa165/model/dto/MatchDto.java
@@ -0,0 +1,19 @@
+package cz.muni.fi.pa165.model.dto;
+
+import cz.muni.fi.pa165.model.dto.TeamDto;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * A DTO for the Match entity
+ */
+public record MatchDto(
+        Long id,
+        Instant dateOfMatch,
+        Integer homeGoals,
+        Integer awayGoals,
+        TeamDto homeTeam,
+        TeamDto awayTeam
+) implements Serializable {
+}
