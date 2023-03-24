26c72934e756d75ee34809f09765d3e9520f7571
Norbert Husarčík <xhusarc@fi.muni.cz>
2023-03-24 19:42:45 +0100

    Added OpenAPI annotations

--- model/src/main/java/cz/muni/fi/pa165/model/dto/MatchDto.java
+++ model/src/main/java/cz/muni/fi/pa165/model/dto/MatchDto.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.pa165.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -7,9 +9,17 @@ import java.time.Instant;
  * A DTO for the Match entity
  */
 public record MatchDto(
+
+        @Schema(example = "1")
         Long id,
+
+        @Schema(example = "2023-04-27 00:00:00-07")
         Instant dateOfMatch,
+
+        @Schema(example = "0")
         Integer homeGoals,
+
+        @Schema(example = "0")
         Integer awayGoals,
         TeamDto homeTeam,
         TeamDto awayTeam
