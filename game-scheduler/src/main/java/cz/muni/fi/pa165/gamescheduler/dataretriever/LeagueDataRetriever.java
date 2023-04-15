90db38d26f3e417ea5d9950151fc13194e1e83fc
Norbert Husarčík <xhusarc@fi.muni.cz>
2023-04-15 21:36:36 +0200

    Moved communication with coreClient in GameSchedulerService to DataRetriever and ApiClient classes

---
+++ game-scheduler/src/main/java/cz/muni/fi/pa165/gamescheduler/dataretriever/LeagueDataRetriever.java
@@ -0,0 +1,31 @@
+package cz.muni.fi.pa165.gamescheduler.dataretriever;
+
+import cz.muni.fi.pa165.model.dto.LeagueDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Component
+public class LeagueDataRetriever {
+
+    public final WebClient coreClient;
+
+    @Autowired
+    public LeagueDataRetriever(WebClient coreClient) {
+        this.coreClient = coreClient;
+    }
+
+    public List<LeagueDto> getLeagues() {
+
+        return coreClient.get()
+                .uri(uriBuilder -> uriBuilder.
+                        pathSegment("api", "leagues").build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<LeagueDto>>() {
+                })
+                .block();
+    }
+}
