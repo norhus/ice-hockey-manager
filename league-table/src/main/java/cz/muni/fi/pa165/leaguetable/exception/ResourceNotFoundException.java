30a4dde6c98bc10ebc11546387ed9c197d9b6227
Ján Homola <xhomola@fi.muni.cz>
2023-04-16 10:35:30 +0200

    Fixed inheritance of ResourceNotFoundException

--- league-table/src/main/java/cz/muni/fi/pa165/leaguetable/exception/ResourceNotFoundException.java
+++ league-table/src/main/java/cz/muni/fi/pa165/leaguetable/exception/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package cz.muni.fi.pa165.leaguetable.exception;
 
-public class ResourceNotFoundException extends Exception {
+public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
         super(message);
